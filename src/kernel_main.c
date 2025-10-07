a677035b4e5f966e70cb598ed4b546c7a618d57b
cchavez09 <chavezchristian910@gmail.com>
2025-10-07 21:19:58 +0000

    Created function with HW2 Deliverables

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -13,6 +13,11 @@ uint8_t inb (uint16_t _port) {
 
 }
 
+// outb function hw2
+void outb (uint16_t _port, uint8_t val) {
+__asm__ __volatile__ ("outb %0, %1" : : "a" (val), "dN" (_port) );
+}
+
 // QEMU definitions
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 25
@@ -68,17 +73,72 @@ int putc(int data) {
     return 0; 
 }
 
+// Homework 2 
+//
+// Keyboard Map
+unsigned char keyboard_map[128] =
+{
+   0,  27, '1', '2', '3', '4', '5', '6', '7', '8',     /* 9 */
+ '9', '0', '-', '=', '\b',     /* Backspace */
+ '\t',                 /* Tab */
+ 'q', 'w', 'e', 'r',   /* 19 */
+ 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', /* Enter key */
+   0,                  /* 29   - Control */
+ 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',     /* 39 */
+'\'', '`',   0,                /* Left shift */
+'\\', 'z', 'x', 'c', 'v', 'b', 'n',                    /* 49 */
+ 'm', ',', '.', '/',   0,                              /* Right shift */
+ '*',
+   0,  /* Alt */
+ ' ',  /* Space bar */
+   0,  /* Caps lock */
+   0,  /* 59 - F1 key ... > */
+   0,   0,   0,   0,   0,   0,   0,   0,
+   0,  /* < ... F10 */
+   0,  /* 69 - Num lock*/
+   0,  /* Scroll Lock */
+   0,  /* Home key */
+   0,  /* Up Arrow */
+   0,  /* Page Up */
+ '-',
+   0,  /* Left Arrow */
+   0,
+   0,  /* Right Arrow */
+ '+',
+   0,  /* 79 - End key*/
+   0,  /* Down Arrow */
+   0,  /* Page Down */
+   0,  /* Insert Key */
+   0,  /* Delete Key */
+   0,   0,   0,
+   0,  /* F11 Key */
+   0,  /* F12 Key */
+   0,  /* All other keys are undefined */
+};
+
+void keyboardScanner() {
+    while(1) {
+        // check if status register changed
+        uint8_t status = inb(0x64);
+        
+        // if status register is 1, then there is scancode
+        if (status & 1) {
+            // get scancode from output buffer
+            uint8_t scancode = inb(0x60);
+            
+            // make sure scancode is greater than 128
+            if (scancode > 128) {
+                continue;
+            }
+
+            // print out scancode and character
+            esp_printf(putc, "0x%02x %c\n", scancode, keyboard_map[scancode]);
+        }
+    }
+}
 
 void main() {
+  
+    keyboardScanner();
     
-
-    // for loop to repeat phrase iterating which line the 
-    // loop is on
-    for (int i = 0; i < 30; i++) {
-        esp_printf(putc, "I am currently on line %d\n", i); 
-    }
-
-
-    while(1);
-
 }
